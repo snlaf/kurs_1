546da30fe2e51edc8d0a0340b14f76613f362f38
snlaf <abokarev539@gmail.com>
2023-02-09 19:47:55 +0300

    Update Продукт.py

--- Продукт.py
+++ Продукт.py
@@ -1,26 +1,20 @@
 import matplotlib.pyplot as pl
 import numpy as nup
 
-def plot_function(ax, func, value):
+def plot_function_2d(func, value):
+    fig, ax = pl.subplots()
     X = nup.linspace(-nup.pi, nup.pi, 256)
-    Y = nup.linspace(-nup.pi, nup.pi, 256)
-    X, Y = nup.meshgrid(X, Y)
-    Z = func(value * nup.sqrt(X**2 + Y**2))
-
-    ax.plot_surface(X, Y, Z, cmap='plasma')
-    ax.set_title("Функция")import matplotlib.pyplot as pl
-import numpy as nup
-
-def plot_function_2d(ax, func, value):
-    X = nup.linspace(-nup.pi, nup.pi, 256)
-    Y = funct(value * X)
-
+    Y = func(value * X)
     ax.plot(X, Y)
+
     ax.set_title("Функция")
     ax.set_xlabel("X координата")
     ax.set_ylabel("Y координата")
+    pl.show()
 
-def plot_function_3d(ax, func, value):
+def plot_function_3d(func, value):
+    fig = pl.figure()
+    ax = fig.add_subplot(111, projection='3d')
     X = nup.linspace(-nup.pi, nup.pi, 256)
     Y = nup.linspace(-nup.pi, nup.pi, 256)
     X, Y = nup.meshgrid(X, Y)
@@ -31,6 +25,7 @@ def plot_function_3d(ax, func, value):
     ax.set_xlabel("X координата")
     ax.set_ylabel("Y координата")
     ax.set_zlabel("Z координата")
+    pl.show()
 
 def funct_x(value):
     return value
@@ -42,9 +37,6 @@ def funct_sqrt(value):
     return nup.sqrt(value)
 
 if __name__ == '__main__':
-    fig = pl.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
     functions = {
         'sin': nup.sin,
         'cos': nup.cos,
@@ -65,41 +57,6 @@ if __name__ == '__main__':
     value = float(input("Введите значение функции: "))
 
     if choice in ['x', 'x^2', 'sqrt(x)']:
-        fig = pl.figure()
-        ax = fig.add_subplot(111)
-        plot_function_2d(ax, funct, value)
+        plot_function_2d(funct, value)
     else:
-        fig = pl.figure()
-        ax = fig.add_subplot(111, project='3d')
-        plot_function_3d(ax, funct, value)
-
-    pl.tight_layout()
-    pl.show()
-
-    ax.set_xlabel("X координата")
-    ax.set_ylabel("Y координата")
-    ax.set_zlabel("Z координата")
-
-if __name__ == '__main__':
-    fig = pl.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    functions = {
-        'sin': nup.sin,
-        'cos': nup.cos,
-        'tan': nup.tan,
-        'exp': nup.exp,
-        'log': nup.log
-    }
-
-    choice = input("Выберете функцию из списка 'sin', 'cos', 'tan', 'exp', 'log': ")
-    funct = functions.get(choice)
-    if funct is None:
-        print("Неверный выбор")
-        exit()
-
-    value = float(input("Введите значение функции: "))
-    plot_function(ax, funct, value)
-
-    pl.tight_layout()
-    pl.show()
+        plot_function_3d(funct, value)
