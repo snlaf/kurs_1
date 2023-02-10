38caae5a847970bdf5cea8272cc114277c9d7000
snlaf <abokarev539@gmail.com>
2023-02-10 18:49:58 +0300

    New functionality. Plus new "infinity method of running)"

--- Продукт.py
+++ Продукт.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as pl
 import numpy as nup
 
+
 def plot_function_2d(func, value):
     fig, ax = pl.subplots()
     X = nup.linspace(-nup.pi, nup.pi, 256)
@@ -12,13 +13,14 @@ def plot_function_2d(func, value):
     ax.set_ylabel("Y координата")
     pl.show()
 
+
 def plot_function_3d(func, value):
     fig = pl.figure()
     ax = fig.add_subplot(111, projection='3d')
     X = nup.linspace(-nup.pi, nup.pi, 256)
     Y = nup.linspace(-nup.pi, nup.pi, 256)
     X, Y = nup.meshgrid(X, Y)
-    Z = func(value * nup.sqrt(X**2 + Y**2))
+    Z = func(value * nup.sqrt(X ** 2 + Y ** 2))
 
     ax.plot_surface(X, Y, Z, cmap='plasma')
     ax.set_title("Функция")
@@ -27,17 +29,22 @@ def plot_function_3d(func, value):
     ax.set_zlabel("Z координата")
     pl.show()
 
+
 def funct_x(value):
     return value
+
 
 def funct_x_squared(value):
     return value ** 2
 
+
 def funct_sqrt(value):
     return nup.sqrt(value)
 
+
 def funct_ctg(value):
     return 1 / nup.tan(value)
+
 
 if __name__ == '__main__':
     functions = {
@@ -52,15 +59,19 @@ if __name__ == '__main__':
         'sqrt(x)': funct_sqrt
     }
 
-    choice = input("Выберете функцию из списка 'sin', 'cos', 'tan', 'ctg', 'exp', 'log', 'x', 'x^2', 'sqrt(x)': ")
-    funct = functions.get(choice)
-    if funct is None:
-        print("Неверный выбор")
-        exit()
-
-    value = float(input("Введите значение функции: "))
-
-    if choice in ['x', 'x^2', 'sqrt(x)']:
-        plot_function_2d(funct, value)
-    else:
-        plot_function_3d(funct, value)
+    while True:
+        choice = input(
+            "Выберете функцию из списка 'sin', 'cos', 'tan', 'ctg', 'exp', 'log', 'x', 'x^2', 'sqrt(x)' или введите 'exit' для выхода: ")
+        if choice == 'exit':
+            break
+        else:
+            funct = functions.get(choice)
+            value = float(input("Введите значение функции: "))
+            dim = input("Выберете кол-во измерений (2 или 3): ")
+            if dim == '2':
+                plot_function_2d(funct, value)
+            elif dim == '3':
+                plot_function_3d(funct, value)
+            else:
+                print("Неверный выбор")
+                exit()
